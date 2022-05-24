3d1672737536abfb7faca744254dd2ed4d02fa48
Pascal <84098807+PascalWebDev@users.noreply.github.com>
2022-05-24 18:16:37 +0200

    Delete Part One directory

--- Part One/Player.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-class Player():
-    def __init__(self, name):
-=======
-class Player():
-    def __init__(self, name):
->>>>>>> 6206c8411e7e92a03b9f02ebdb19c732a9093a01
-        self.name = name
